b9b38baee2ed3b0905d6c1e31f9e9bd07cf774f4
Ye Tun <yetun71098@gmail.com>
2023-02-11 12:12:52 +0630

    arithmetic_operation

---
+++ data-types/arithemetic_operation.py
@@ -0,0 +1,58 @@
+# # # num1 = 1/2 #စားလတ်ကိုယူ
+# # # num2  =5%2 #အကြွင်းယူ
+# # # expo = 2 ** 2 #front base and back power
+# # # print(f"num 1 is {num1} and {num2} {expo}")
+
+# # # #assignment operator
+# # # num1 =5
+# # # num1 = num1 + 2
+# # # num1 /= 3 
+# # # print(num1)
+# # # #comparison operators
+# # # #equal == x==y
+# # # #not equal != x!=y
+# # # #Greater than > x>y
+# # # #less than < x<y
+# # # #greater than equal x>=y
+
+# # # num1=5 
+# # # num2=3
+# # # print(num1 == num2)
+
+# # # #logical operator
+# # # #and or not
+# # # print(num1 > 0 or num2 < 0)
+
+# # #type cast
+# # # num1=input("please enter number")
+# # # num1=int(num1)
+# # # num2=int(input("please enter number"))
+# # # print(num1+num2)
+#int (false) 0
+#bool(1) true
+
+# # #esacape characters
+# # #Double Quote  \""
+# # #New Line  \n
+# # #tab \t
+# # #blackslash \\
+# # #This is a "string"
+
+# # chars = "acbc" *2
+
+# # # # print("This is  a \"string\"")
+
+# # # fruit="strawberry"
+# # # print(f"\"Best Fruit is : {fruit}\" \n \"Yes, {fruit}. Only {fruit}\" \n \"Hello, my namme is Mr.{fruit}\"\n\"fruit = \'{fruit}\'")
+
+# # fruit="strawberry"
+# # print(f"Hello {fruit}")
+
+# #swap variables
+
+# x=2
+# y=3
+# z=5
+# y,x,*rest=x,y,z,5 #rest can be place front and back only not middle
+# print(x)
+
