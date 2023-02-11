d5c579b9206e901c434ab0514c365bd02d8fbff8
Ye Tun <yetun71098@gmail.com>
2023-02-11 12:01:17 +0630

    Variables

---
+++ data-types/variables.py
@@ -0,0 +1,12 @@
+x = 1
+y = 2
+str = "Today is Sunday"
+print (f" {x} and {y} {str}")
+
+print(type(x))
+# special character :;""*&^%$@!
+# key word class print သုံးမရ
+# variable type တွေကြေငြာစရာမလိုပါ
+"""
+f is format string
+"""
