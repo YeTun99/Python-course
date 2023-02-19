b1059a974021c200b6f543ac449d4838ef33d9af
Ye Tun <yetun71098@gmail.com>
2023-02-19 11:25:07 +0630

    new

---
+++ conditional_statement/exerciseif.py
@@ -0,0 +1,18 @@
+# score=int(input("Enter Score:"))
+# if score >=90 and score<=100:
+#     print("Grade A")
+# elif score >=80 and score<90:
+#     print("Grade B")
+# elif score >=70 and score<80:
+#     print("Grade C")
+# elif score >=40 and score<70:
+#     print("Grade D")
+# elif score >100:
+#     print("Out of range")
+# else:
+#     print("Fail")
+
+a=20
+b=20
+result= "a" if  a>b else "b"
+print(result)
---
+++ conditional_statement/loop.py
@@ -0,0 +1,24 @@
+#for variable in sequence:
+    #do block of coe 
+        #print("Hello Worlf")
+#while condition (True):
+    #do block of code 
+    #print("Hello World")
+
+a=1
+while a<5:
+    print("Hello World")
+    a+=1
+
+my_list=[1,2,3,4,5]
+for variables in my_list:
+    print(variables)
+
+for my_var in range(len(my_list)):
+    print(my_list[my_var])
+
+while True:
+    n=int(input("Enter positive number"))
+    if n>0:
+        print(n)
+        break
